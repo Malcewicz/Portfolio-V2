c8158eea398f95842124adf696dc08073c91960b
Maciej Bernatowicz <98919214+Malcewicz@users.noreply.github.com>
2024-05-29 01:17:13 +0200

    Created hero section

---
+++ app/_components/hero.tsx
@@ -0,0 +1,28 @@
+import styles from "@/app/_styles/hero.module.css";
+
+const Hero = () => {
+  return (
+    <header className={styles.hero}>
+      <div className={styles.container}>
+        <h3 className={styles.subheading}>Hello! I'm Maciej</h3>
+        <h1 className={styles.heading}>
+          Transforming Ideas into Reality through{" "}
+          <span>Technology and Cooperation</span>
+        </h1>
+        <p className={styles.description}>
+          With a foundation in software development and a passion for project
+          management, I bring a unique blend of technical expertise and
+          strategic oversight to every project, turning complex challenges into
+          seamless solutions.
+        </p>
+      </div>
+
+      <div className={styles.background}>
+        <div className={styles.circle_top_right}></div>
+        <div className={styles.circle_btm_left}></div>
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
