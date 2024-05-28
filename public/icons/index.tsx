f7690df1fb1493a3803f563608cfff52287d8ac4
Maciej Bernatowicz <98919214+Malcewicz@users.noreply.github.com>
2024-05-29 01:16:20 +0200

    Added icons

---
+++ public/icons/index.tsx
@@ -0,0 +1,7 @@
+export { default as Logo } from "./logo";
+export { default as CommunicationIcon } from "./communication-icon";
+export { default as FlexibilityIcon } from "./flexibility-icon";
+export { default as LearningIcon } from "./learning-icon";
+export { default as PeopleOrientedIcon } from "./people-oriented-icon";
+export { default as ProblemSolvingIcon } from "./problem-solving-icon";
+export { default as QualityIcon } from "./quality-icon";
