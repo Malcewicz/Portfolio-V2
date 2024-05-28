63b55eedcfca26b7a96c757ccd2bda1f78d76cae
Maciej Bernatowicz <98919214+Malcewicz@users.noreply.github.com>
2024-05-29 01:20:27 +0200

    Added index for better imports

---
+++ app/_components/index.tsx
@@ -0,0 +1,9 @@
+export { default as Navbar } from "./ui/navbar";
+export { default as Footer } from "./footer";
+export { default as Hero } from "./hero";
+export { default as AboutMe } from "./about-me";
+export { default as Projects } from "./projects";
+export { default as Experience } from "./experience";
+export { default as Values } from "./values";
+export { default as Contact } from "./contact";
+export { default as ProjectCursor } from "./ui/project-cursor";
